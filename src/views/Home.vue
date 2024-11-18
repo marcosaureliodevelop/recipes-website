2869c0cc9812303bbc0ca7ae8c9326c5b1409775
Marcos <aurliomarcos@gmail.com>
2024-11-18 17:46:57 -0300

    arch: adjust folders structure

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+	<main class="homepage">
+		<h1>Hello World!</h1>
+	</main>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+export default class HomeView extends Vue {}
+</script>
